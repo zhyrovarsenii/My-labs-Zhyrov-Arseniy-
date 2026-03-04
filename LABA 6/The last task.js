48cba62900135d9e93e9318548a37b1f5cc16a38
zhyrovarsenii <zhyrov.arsenii@krfk.kai.edu.ua>
2026-03-04 11:00:02 +0200

    Last tasks for 6 and 7 labs

---
+++ LABA 6/The last task.js
@@ -0,0 +1,4 @@
+// Додаткове завдання на 5 балів. Написати програму, яка перетворює масив рядків у один об'єднаний рядок.
+let arrayOfStrings = ["Привіт", "світ", "JavaScript"];
+let result = arrayOfStrings.join(" ");
+console.log(result);
